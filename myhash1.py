41917e0b0f3a01685ca48cd414572517e172a40c
Tom Tarpey <tomtarpeydev@gmail.com>
2020-05-05 09:41:23 +0100

    collisions

--- myhash1.py
+++ myhash1.py
@@ -13,35 +13,40 @@ def my_hash(s):
 
 # hash the index
 def hash_index(key):
-    pass
+    h = my_hash(key)
+    return h % len(hash_table)
 
 # put
 def put(key, val):
-    pass
+    i = hash_index(key)
+    if hash_table[i] != None:
+        print(f"Collision! Overwriting {repr(hash_table[i])}")
+    hash_table[i] = val
 
 # get
 def get(key):
-    pass
+    i = hash_index(key)
+    return hash_table[i]
 
 # delete
 def delete(key):
-    pass
+    i = hash_index(key)
+    hash_table[i] = None
 
 
 if __name__ == "__main__":
 
-    # put("Hello", "Hello Value")
-    # put("World", "World Value")
-    # put("foo", "foo value")   # "foo" hashes to same index as "Hello"
-    #                         # AKA "foo collides with Hello"
+    put("Hello", "Hello Value")
+    put("World", "World Value")
+    put("foo", "foo value")   # "foo" hashes to same index as "Hello"
+                            # AKA "foo collides with Hello"
 
-    # print(hash_table)
+    print(hash_table)
 
-    # v = get("Hello")
-    # print(v) # "Hello Value"
+    v = get("Hello")
+    print(v) # "Hello Value"
 
-    # # Get "Frogs" from the table
-    # # Doesn't exist!
-    # v = get("Frogs")
-    # print(v)  # "None"
-    pass
+    # Get "Frogs" from the table
+    # Doesn't exist!
+    v = get("Frogs")
+    print(v)  # "None"
