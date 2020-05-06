94cd8b11ac39aaff2573e0061a23f8bf42cd7b87
Tom Tarpey <tomtarpeydev@gmail.com>
2020-05-06 10:37:51 +0100

    first pass webcache

--- webcache.py
+++ webcache.py
@@ -1,8 +1,69 @@
+"""
+- Specification Abstract
+    - We want a web client that gets whichever URL we specify
+    - it caches the results of the web page
+    - the first time we visit a URL, it goes out over the internet and gets it
+        - subsequent requests for that URL are simply returned from the cache
+
+- what benefit is this?
+    - example. proxy server in a big company office whare we have a company firewall that caches data
+        - in order to minimize traffic we can make the firewall in to a proxy cache server
+
+- how can we use a hash table to create this?
+    - The `URL` is the `key`
+    - The web page `data` is the `value`
+        - for our purposes the data will be a webpage
+
+
+
+"""
+
+
 # what imports will we need
+# https://docs.python.org/3/library/urllib.request.html#module-urllib.request
+import urllib.request
 
 # a class to hold each entry
-
+class CacheEntry:
+    """
+    hold info about the cache entry
+    - url
+    - data
+    """
+    def __init__(self, url, data):
+        self.url = url
+        self.data = data
 
 # a cache
+cache = {}
 
-# how long will we keep each item in the cache?
+# how long will we keep each item in the cache? TODO:
+
+# make a REPL
+while True:
+    # take input from user
+    url = input("Enter a URL: ")
+
+    # set some data to None
+    data = None
+
+    # check if a key is in the cache
+    if url in cache:
+        # set our entry to the cache at key
+        entry = cache[url]
+        # set data to entry.data
+        data = entry.data
+
+    # check if data is None
+    if data is None:
+        print("Getting from Server!")
+
+        # open a connection to read data
+        res = urllib.request.urlopen(url)
+        data = res.read()
+
+        # store the data in the cache (wrapping the data in a CacheEntry object)
+        cache[url] = CacheEntry(url, data)
+
+        # close the connection
+        res.close()
